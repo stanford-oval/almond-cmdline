497f947429e5ce2eebfd47377eaba12e4ad5db2c
Giovanni Campagna <gcampagn@cs.stanford.edu>
2017-08-03 17:23:31 -0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of ThingEngine
+//
+// Copyright 2017 Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+
+module.exports.THINGPEDIA_URL = 'https://thingpedia.stanford.edu/thingpedia';
+module.exports.SEMPRE_URL = 'https://sabrina-nl.stanford.edu';

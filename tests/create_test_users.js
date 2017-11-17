8d03528ce6ec36ff5995b4cb30f73ea6bd31d354
Giovanni Campagna <gcampagn@cs.stanford.edu>
2017-11-17 14:47:39 -0800

    Add tests to manage large rooms and large number of fake users
    Against our home server

---
+++ tests/create_test_users.js
@@ -0,0 +1,85 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of ThingEngine
+//
+// Copyright 2016-2017 Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+"use strict";
+
+const Q = require('q');
+Q.longStackSupport = true;
+
+const uuid = require('uuid');
+const fs = require('fs');
+const path = require('path');
+const events = require('events');
+const readline = require('readline');
+const ThingTalk = require('thingtalk');
+
+const Config = require('../config');
+
+const Engine = require('thingengine-core');
+
+const TEST_HOMESERVER = 'camembert.stanford.edu';
+
+process.env.MATRIX_IDENTITY_SERVER_URL = `http://${TEST_HOMESERVER}:8090`;
+process.env.MATRIX_HOMESERVER_URL = `http://${TEST_HOMESERVER}:8008`;
+
+function initUser(i) {
+    const delegate = {
+        reply(msg) {
+            console.log('>> ' + reply);
+        },
+        confirm(question) {
+            console.log('>? ' + question);
+            return Q(true);
+        },
+        requestCode(question) {
+            console.log('>= ' + question);
+
+            switch (question) {
+            case "Insert your email address or phone number:":
+                return Q(`testuser${i}@${TEST_HOMESERVER}`);
+            case "Insert your password:":
+                return Q(`testuser${i}`);
+            default:
+                throw new Error('Unexpected question');
+            }
+        }
+    };
+
+    let homedir = './home-testuser' + i;
+    console.log('Initializing user ' + i);
+
+    let platform = require('../platform').newInstance(homedir);
+    let engine = new Engine(platform, { thingpediaUrl: process.env.THINGPEDIA_URL || Config.THINGPEDIA_URL });
+    engine.__engineId = i;
+
+    return engine.open().then(() => {
+        return engine.devices.factory.getFactory('org.thingpedia.builtin.matrix');
+    }).then((factory) => {
+        return factory.configureFromAlmond(engine, delegate);
+    }).then((device) => {
+        return Q.delay(2000);
+    }).then((device) => {
+        console.log('ok! Configured user ' + i);
+        return engine.close();
+    });
+}
+
+function promiseLoop(begin, end, fn) {
+    return (function loop(i) {
+        if (i === end)
+            return Q();
+
+        return Q(fn(i)).then(() => {
+            return loop(i+1);
+        });
+    })(begin);
+}
+
+function main() {
+    promiseLoop(1, 11, initUser).then(() => process.exit()).done();
+}
+main();
---
+++ tests/invite_all_in_room.js
@@ -0,0 +1,76 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of ThingEngine
+//
+// Copyright 2016-2017 Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+"use strict";
+
+const Q = require('q');
+Q.longStackSupport = true;
+
+const uuid = require('uuid');
+const fs = require('fs');
+const path = require('path');
+const events = require('events');
+const readline = require('readline');
+const ThingTalk = require('thingtalk');
+
+const Config = require('../config');
+
+const Engine = require('thingengine-core');
+
+const TEST_HOMESERVER = 'camembert.stanford.edu';
+
+process.env.MATRIX_IDENTITY_SERVER_URL = `http://${TEST_HOMESERVER}:8090`;
+process.env.MATRIX_HOMESERVER_URL = `http://${TEST_HOMESERVER}:8008`;
+
+function initUser(i) {
+    let homedir = './home-testuser' + i;
+    console.log('Initializing user ' + i);
+
+    let platform = require('../platform').newInstance(homedir);
+    let engine = new Engine(platform, { thingpediaUrl: process.env.THINGPEDIA_URL || Config.THINGPEDIA_URL });
+    engine.__engineId = i;
+
+    return engine.open().then(() => engine);
+}
+
+function promiseLoop(begin, end, fn) {
+    let results = [];
+    return (function loop(i) {
+        if (i === end)
+            return Q(results);
+
+        return Q(fn(i)).then((res) => {
+            results.push(res);
+            return loop(i+1);
+        });
+    })(begin);
+}
+
+
+function main() {
+    let name = process.argv[2];
+    if (!name)
+        throw new Error('Specify room name in command line');
+    let begin = parseInt(process.argv[3]);
+    let end = parseInt(process.argv[4]);
+    if (!(begin < end))
+        throw new Error('Invalid begin/end');
+
+    promiseLoop(1, 11, initUser).then((engines) => {
+        let leader = engines[0];
+        let followers = [];
+        for (let i = begin; i <= end; i++)
+            followers.push(`@testuser${i}:${TEST_HOMESERVER}`);
+
+        let messaging = leader.messaging._messagingIface;
+
+        return messaging.client.createRoom({ visibility: 'private', preset: 'private_chat', room_alias_name: name, invite: followers }).then(() => {
+            return Q.all(engines.map((e) => e.close()));
+        });
+    }).then(() => process.exit()).done();
+}
+main();
